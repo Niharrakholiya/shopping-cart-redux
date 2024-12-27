f0b2166b64c53c78ef63bcbc50949c1ca0b99942
Niharrakholiya <nihar.rakholiya526@gmail.com>
2024-12-27 19:08:48 +0530

    Bug fixes

--- src/redux/slices/productslice.ts
+++ src/redux/slices/productslice.ts
@@ -1,6 +1,6 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
-
+import { Product } from "@/types/index";
 // Thunks
 export const fetchProducts = createAsyncThunk("products/fetchProducts", async () => {
     const response = await axios.get("https://fakestoreapi.com/products");
